9b296a7451b6e30b9cd0c72ce38ca84a96556357
arucard21 <arucard21@gmail.com>
2024-12-24 16:14:19 +0100

    Update script to install applications in the correct order, waiting for their completion, and ensuring that the ingresses get configured correctly.

--- setup.sh
+++ setup.sh
@@ -8,24 +8,29 @@
 # - base64 (non-critical for cluster setup)
 #
 
-# Create kubernetes cluster (can be skipped if a kubernetes cluster is already available)
+# Create kubernetes cluster
 #
 # Creates a single server node for a simple but non-high-availability configuration
 # Disables the default loadbalancer that distributes traffic to multiple nodes since we only have one node
 k3d cluster create --no-lb --k3s-arg="--disable=traefik@server:0" personal-cloud
 
-# Install Argo CD
-helm install argo-cd argo-cd/ --create-namespace --namespace argocd --version 7.7.11 --dependency-update --wait
-# Add Argo CD as application in Argo CD so it can also be managed from there
-kubectl apply -f argo-cd.yaml --wait
+# Install Argo CD without ingress
+helm install argo-cd argo-cd/ --create-namespace --namespace argocd --version 7.7.11 --dependency-update --wait --set "argo-cd.server.ingress.enabled=false" --set "argo-cd.server.ingressGrpc.enabled=false"
 
 # Install Istio to ensure access to Argo CD and other applications from outside the cluster
-kubectl apply -f istio.yaml --wait
+kubectl apply -f istio.yaml
+kubectl wait --for="jsonpath={.status.health.status}=Healthy" application/istio -n argocd --timeout=60s
 # Install Istio gateway separately since its installation will fail if Istio is not completely installed yet.
-kubectl apply -f istio-gateway.yaml --wait
+kubectl apply -f istio-gateway.yaml
+kubectl wait --for="jsonpath={.status.health.status}=Healthy" application/istio-gateway -n argocd --timeout=60s
+
+# Add Argo CD as application in Argo CD so it can also be managed from there. This also configures the ingress which should now work with Istio.
+kubectl apply -f argo-cd.yaml --wait
+kubectl wait --for="jsonpath={.status.health.status}=Healthy" application/argo-cd -n argocd --timeout=60s
 
 # Install the personal cloud suite of applications
-kubectl apply -f personal-cloud.yaml --wait
+kubectl apply -f personal-cloud.yaml
+kubectl wait --for="jsonpath={.status.health.status}=Healthy" application/nextcloud -n argocd --timeout=60s
 
 # Provide information needed to access the Argo CD UI
 initialPassword=`kubectl get secret -n argocd argocd-initial-admin-secret -o "jsonpath={.data.password}" | base64 -d`
@@ -38,10 +43,6 @@ echo "\tnextcloud.personal.cloud"
 echo
 echo "Argo CD should now be available at http://argocd.personal.cloud"
 echo "You can log in with username admin and password $initialPassword"
-
-
-# Provide instructions to install the personal-cloud app
 echo
-echo "You can install the personal cloud applications through the UI or with the following command in this repo"
-echo
-echo "kubectl apply -f personal-cloud.yaml"
+echo "Nextcloud should now be available at http://nextcloud.personal.cloud"
+echo "You can log in with username admin and password changeme"
